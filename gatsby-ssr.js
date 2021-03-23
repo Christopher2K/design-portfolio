c76c078dd2ecaa149c9a2553d07dba1981b52a77
Christopher N. KATOYI <me@christopher2k.dev>
2021-03-23 16:06:53 +0100

    :bug: fix ssr on local dev

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { Layout } from './src/components'
+import { theme } from './src/styles/theme'
+import { Global } from './src/styles/global'
+
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>
+}
+
+export function wrapRootElement({ element }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Global />
+      {element}
+    </ThemeProvider>
+  )
+}
